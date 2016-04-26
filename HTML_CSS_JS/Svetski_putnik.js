6867ebbf62ded821665c7965ab60807310f1bfc2
JelicaC <jelica94@rocketmail.com>
2016-04-26 07:34:17 +0200

    HTML-CSS-JS

---
+++ HTML_CSS_JS/Svetski_putnik.js
@@ -0,0 +1,39 @@
+
+function f() {
+    location.href = "Izaberi nivo.html";
+};
+
+
+function show1(){
+	document.getElementById("d1").style.display='inline-block';
+	document.getElementById("b11").disabled=true;
+	document.getElementById("b12").disabled=true;
+
+}
+
+function show2(){
+	document.getElementById("d2").style.display='inline-block';
+	document.getElementById("b11").disabled=true;
+	document.getElementById("b12").disabled=true;
+
+}
+
+function close1(){
+	document.getElementById("d1").style.display='none';
+	document.getElementById("b11").disabled=false;
+	document.getElementById("b12").disabled=false;
+}
+
+function close2(){
+	document.getElementById("d2").style.display='none';
+	document.getElementById("b11").disabled=false;
+	document.getElementById("b12").disabled=false;
+}
+
+function alertf1(){
+	alert("Proverava se da li je uspesna prijava");
+}
+
+function alertf2(){
+	alert("Proverava se da li je uspesna registracija");
+}
