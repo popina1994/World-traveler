b47c936a9e8360f718d077f5d3899f804707b3bb
JelicaC <jelica94@rocketmail.com>
2016-06-20 00:58:36 +0200

    dodat slavkov test i svi logovi

---
+++ source/application/tests/models/TestSlavko.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: slavko
+ * Date: 6/20/2016
+ * Time: 12:48 AM
+ */
+class TestSlavko extends TestCase
+{
+    public function setUp() {
+        $_SESSION['username'] = 'guest';
+
+    }
+    public function test_DateExist() {
+        $output = $this->request("POST", "Game/checkOldGame", ['secret'=>true]);
+        $this->assertContains("dataExists\":false", $output);
+    }
+
+    public function test_Move() {
+        $_SESSION['correctAnswer'] = 1;
+        $output = $this->request("POST", "Game/getTextAnswer", ['secret'=>true, 'letter'=>'a']);
+        $this->assertContains("correct", $output);
+    }
+
+}
