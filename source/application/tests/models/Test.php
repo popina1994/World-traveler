f5f758a2a35978f9c2336d8621e5a918b6f39913
Ђорђе Живановић <djordjezivanovicva@live.com>
2016-06-19 19:55:24 +0200

    Сређене ствари.

---
+++ source/application/tests/models/Test.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: popina
+ * Date: 19.06.2016.
+ * Time: 19:37
+ */
+class Test extends TestCase
+{
+    public function RunShit() {
+        $this->assertTrue(true);
+    }
+
+    public function RunShit2() {
+        $this->assertTrue(false);
+    }
+}
