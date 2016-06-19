0aaf259e18c7e06f5758ce51d4f5a3e59f765f0f
Ђорђе Живановић <djordjezivanovicva@live.com>
2016-06-19 22:27:01 +0200

    Нови тест.

--- source/application/tests/models/TestText.php
+++ source/application/tests/models/TestText.php
@@ -25,6 +25,9 @@ class TestText extends TestCase
         $output = $this->request("POST", "ModeratorController/inputValidationTekstPitanje", ['secret'=>true, 'o1'=>'Slavko', 'o2'=>'Dragana', 'o3' =>'Dorde', 'o4'=>'Jelica',
             'tacan'=>'1', 'postavka'=>'konj', 'nivo' => 'Beba1', 'oblast'=>'1', 'id' =>'3']);
         $this->assertContains("error\":\"\"", $output);
+        $output = $this->request("POST", "ModeratorController/inputValidationTekstPitanje", ['secret'=>true, 'o1'=>'Slavko', 'o2'=>'Dragana', 'o3' =>'Dorde', 'o4'=>'Jelica',
+            'tacan'=>'1', 'postavka'=>'konj', 'nivo' => '', 'oblast'=>'1', 'id' =>'3']);
+        $this->assertContains("error\":\"\"", $output);
     }
 
 }
