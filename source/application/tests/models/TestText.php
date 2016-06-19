74f9944a126ad846245476789845fd728338f5aa
Ђорђе Живановић <djordjezivanovicva@live.com>
2016-06-19 22:51:21 +0200

    Напредују тестови

--- source/application/tests/models/TestText.php
+++ source/application/tests/models/TestText.php
@@ -19,7 +19,7 @@ class TestText extends TestCase
         $this->objTxt = $this->CI->modeltekstpitanje;
     }
 
-
+/*
     public function test_InputValidation()
     {
         $output = $this->request("POST", "ModeratorController/inputValidationTekstPitanje", ['secret'=>true, 'o1'=>'Slavko', 'o2'=>'Dragana', 'o3' =>'Dorde', 'o4'=>'Jelica',
@@ -28,6 +28,13 @@ class TestText extends TestCase
         $output = $this->request("POST", "ModeratorController/inputValidationTekstPitanje", ['secret'=>true, 'o1'=>'Slavko', 'o2'=>'Dragana', 'o3' =>'Dorde', 'o4'=>'Jelica',
             'tacan'=>'1', 'postavka'=>'konj', 'nivo' => '', 'oblast'=>'1', 'id' =>'3']);
         $this->assertContains("error\":\"\"", $output);
+    }
+*/
+    public function test_CreateText() {
+        $this->assertFalse($this->objTxt->createTekstPitanje(['idniv'=>'Beba', 'idobl'=>'Aljaska', 'idkor' =>'1', 'odgovor1'=>'a', 'odgovor2'=>'b', 'odgovor3'=>'c', 'odgovor4'=>'d',
+            'tacan'=>'1', 'postavka'=>'postavka'
+        ]));
+
     }
 
 }
